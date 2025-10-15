9d1aa01fdaef0def218958eade24fe24f00c955e
webdev-dave <elidovrich.coding@gmail.com>
2025-10-15 15:49:02 -0400

    first commit

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,14 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [__dirname],
+  resolver: {
+    blockList: [
+      /node_modules\/.*\/android\/build\/.*/,
+      /node_modules\/.*\/android\/.cxx\/.*/,
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
